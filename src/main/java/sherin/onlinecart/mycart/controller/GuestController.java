d0ab6ae374078729f8f76a13f0de199898c0bd96
Sherin KJ <kj.sherin@gmail.com>
2025-01-30 20:07:59 +0530

    Database corrected

--- src/main/java/sherin/onlinecart/mycart/controller/GuestController.java
+++ src/main/java/sherin/onlinecart/mycart/controller/GuestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import sherin.onlinecart.mycart.model.Customer;
 import sherin.onlinecart.mycart.service.CustomerService;
 
@@ -26,15 +27,27 @@ public class GuestController {
     @PostMapping("/login")
     public String loginCheck(@RequestParam String userName, @RequestParam String password, Model model,
             HttpServletRequest req) {
-        System.out.println(userName + ", " + password);
+        // System.out.println(userName + ", " + password);
         Customer c = service.loginCheck(userName, password);
-        System.out.println(c);
+        if (c != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("uid", c.getId());
+            session.setAttribute("name", c.getUserName());
+            session.setAttribute("type", c.getUserType());
+            return "redirect:/" + c.getUserType();
+            // System.out.println(c.getUserType());
+        } else {
+            model.addAttribute("msg", "Invalid User Name or Password");
+        }
         return "guest/login";
     }
 
     @GetMapping("/logout")
-    public String logout() {
-        return "guest/login";
+    public String logout(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        // session.removeAttribute("uid");
+        session.invalidate();
+        return "redirect:/login";
     }
 
     @Autowired
