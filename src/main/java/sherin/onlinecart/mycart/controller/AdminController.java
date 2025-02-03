9b49e025ab1bbd693f078f35d30eabc48b6fca9c
Sherin KJ <kj.sherin@gmail.com>
2025-02-03 19:17:27 +0530

    Customer create in admin completed

--- src/main/java/sherin/onlinecart/mycart/controller/AdminController.java
+++ src/main/java/sherin/onlinecart/mycart/controller/AdminController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
+import sherin.onlinecart.mycart.model.Customer;
 import sherin.onlinecart.mycart.service.CustomerService;
 
 @Controller
@@ -15,12 +18,23 @@ public class AdminController {
     @GetMapping("")
     public String home(Model model, HttpSession session) {
         model.addAttribute("session", session);
-        // model.addAttribute("data", service.read());
+        model.addAttribute("data", service.read());
         return "admin/home";
     }
 
     @GetMapping("/create")
     public String create() {
+        return "/admin/create";
+    }
+
+    @PostMapping("/create")
+    public String createPost(@ModelAttribute Customer customer, Model model) {
+        Customer c = service.create(customer);
+        if (c == null) {
+            model.addAttribute("msg", "Not able to create ! Try later !");
+        } else {
+            model.addAttribute("msg", c.getName() + " Registered !");
+        }
         return "/admin/create";
     }
 
